529336c6267b0631575b0f6a1018988343ed83e5
shrish0 <shrishgupta1234@gmail.com>
2024-08-11 21:20:36 +0530

    Java array Merge Sorted Array in Single array

---
+++ JavaArray/MergeSortedArrayInSingleArray.java
@@ -0,0 +1,31 @@
+public class MergeSortedArrayInSingleArray{
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m - 1; // Pointer for nums1
+        int q = n - 1; // Pointer for nums2
+        int index = m + n - 1; // Pointer for the end of nums1
+        while(p>=0 & q>=0){
+            if(nums1[p]>nums2[q]){
+                nums1[index]=nums1[p];
+                p--;
+            }
+            else{
+                nums1[index]=nums2[q];
+                q--;
+            }
+            index--;
+        }
+        while(q>=0){
+            nums1[index--]=nums2[q--];
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,4,7,9,12,0,0,0};
+        int[] nums2 = {2,4,21};
+        merge(nums1, 5, nums2, 3);
+        System.out.println("Merged Array:");
+        for(int x : nums1){
+            System.out.print(x+" ");
+        }
+    }
+}
